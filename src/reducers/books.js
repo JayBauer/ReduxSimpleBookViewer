61f115ef6891fb4e2638fc95070af9827e813a23
Jay Bauer <jbauer@elitedigital.ca>
2017-06-26 17:25:18 -0400

    React/Redux Book Details Viewer Functional

---
+++ src/reducers/books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript', pages: 50 },
+    { title: '\'Arry Pottah', pages: 1200 },
+    { title: 'Lawds of Them Riiiings', pages: 3000000 },
+    { title: 'Eloquent Ruby', pages: 1 }
+  ]
+}
